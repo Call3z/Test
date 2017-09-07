ee0473de39a65697ab4f20e25b0846575cb41a02
Call3z <electrodez@gmail.com>
2017-09-07 11:10:10 +0200

    added categories tv dvd vhs

--- Test/Data/Seed.cs
+++ Test/Data/Seed.cs
@@ -10,10 +10,11 @@ namespace Test.Data
     {
         public static void Initialize(ApplicationDbContext context)
         {
-            var category1 = new ProductCategory() { Name = "Telefon" };
-            var category2 = new ProductCategory() { Name = "Tv" };
-            var product1 = new Product() { Name = "Samsung galaxy", Price = 1000, Category = category1 };
-            var product2 = new Product() { Name = "Sony SmartTv", Price = 1000, Category = category2 };
+            var category1 = new ProductCategory() { Name = "TV" };
+            var category2 = new ProductCategory() { Name = "DVD" };
+            var category3 = new ProductCategory() { Name = "VHS" };
+            var product1 = new Product() { Name = "Samsung SmartTv", Price = 1000, Category = category1 };
+            var product2 = new Product() { Name = "Sony SmartTv", Price = 1000, Category = category1 };
 
             context.Products.AddRange(product1, product2);
             context.SaveChanges();
